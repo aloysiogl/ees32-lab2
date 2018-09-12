7e4810a155a170ab3964048f7d1fdcccf18cfccf
Unknown <aloysiogl@gmail.com>
2018-09-12 16:49:01 -0300

    Finished distance finder

--- DistanceFinder.py
+++ DistanceFinder.py
@@ -1,24 +1,33 @@
 import numpy as np
 
 class DistanceFinder:
+
     def __init__(self):
         self.inputs = []
         self.outputs = []
-        self.G = None
+        self.g = None
         self.k = None
         self.n = None
-        self.distance = None
+        self.dist = None
 
     def set_g(self, g):
         """Sets the G matix"""
         self.k, self.n = g.shape
+        self.g = g
         self.__calculate_distance()
 
     def __calculate_distance(self):
-        for i in range(2**self.k):
+        for i in range(1, 2**self.k):
             inp = [(i >> x) % 2 for x in range(self.k)]
-            print(inp)
-            self.inputs.append([])
+            self.inputs.append(np.array([inp]))
+
+        dist = np.sum(np.mod(np.dot(self.inputs[0], self.g), 2))
+
+        for inp in self.inputs:
+            dist = min(dist, np.sum(np.mod(np.dot(inp, self.g), 2)))
+
+        self.dist = dist
 
     def get_distance(self):
-        return self.distance
+        return self.dist
+
--- distance_finder.py
+++ distance_finder.py
@@ -3,7 +3,14 @@ from DistanceFinder import DistanceFinder
 
 if __name__ == "__main__":
     dist = DistanceFinder()
-    a = np.array([[1,1],
-                  [1,3],
-                  [1,4]],)
+    a = np.array([[1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+                  [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+                  [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
+                  [0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+                  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]],)
+
+    
+
     dist.set_g(a)
+    print(dist.get_distance())
